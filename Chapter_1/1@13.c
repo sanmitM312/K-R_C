27a7a4b5ea2d710e31ce421158abd23eed24ea68
sanmitM312 <sanmitmandal17@gmail.com>
2024-08-04 23:57:06 +0530

    1@13.c

--- Chapter_1/1@13.c
+++ Chapter_1/1@13.c
@@ -34,8 +34,53 @@ void horizontal_histogram(){
 		}
 	}
 }
+
+void vertical_histogram(){
+	int wl[21]; // taking max word length to be 20.
+	
+	int c, state, cc = 0;
+	
+	state = OUT;
+
+	while((c = getchar()) != '\n' && c != EOF){
+		if(c == ' ' || c == '\t' || c == '\n'){
+			if(state == IN){
+				wl[cc] += 1;
+				cc = 0;
+			}
+			state = OUT;
+		}else{
+			if(state == OUT){
+				state = IN;
+			}else{
+				cc++;
+			}
+			
+		}
+	}
+	
+	printf("\n3 \'|||\' equals 1 count. [LENGTH]: [COUNT]\n");
+	for(int i = 0; i < 21; i++){
+		printf("\n%4d:",i+1);
+		for(int k=0; k < wl[i]; k++){
+			printf("|||");
+		}
+	}
+/*	
+	printf("\n");
+	for(int k=25; k >=0; k--){
+		for(int l=0; l < 21; l++){
+			if(k<=wl[l]){
+				printf("OK%d%d ",k,wl[l]);
+			}
+		}
+		printf("\n");
+	}
+*/
+}
 int main(){
 
-	horizontal_histogram();
+	//horizontal_histogram();
+	vertical_histogram();
 	
 }
