1a4954caf64d39bf6dca084e795f78bd8a8ea2d3
sanmitM312 <sanmitmandal17@gmail.com>
2024-08-13 00:01:18 +0530

    13/8/24

--- Chapter_2/2@2.c
+++ Chapter_2/2@2.c
@@ -3,4 +3,23 @@
 int main(){
 	
 	// code for 2.2
+	
+	int c;
+	
+	int lim = 10, i = 0;
+	char s[lim];
+	for(int i=0; i<10; i++)s[i] = ' ';
+	
+	while(i < lim-1){
+		c = getchar();
+		if(c != EOF){
+		    if(c != '\n'){
+		    	s[i] = c;
+		    }	
+		}
+		++i;
+	}
+	s[i] = '\0';
+	
+	printf("\nO/P : %s  ", s);
 }
