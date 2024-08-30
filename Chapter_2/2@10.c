472601ca1189c7bb775aac0eaaa7d9aa8a2cb038
sanmitM312 <sanmitmandal17@gmail.com>
2024-08-31 00:11:48 +0530

    31/8/24

--- Chapter_2/2@10.c
+++ Chapter_2/2@10.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
 
+void lower_ascii(char* str){
+	int i;
+	for(i=0; str[i] != '\0'; i++){
+		if(str[i] >= 'A' && str[i] <='Z'){
+			str[i] = str[i] + 32;
+		}
+	}
+}
 int main(){
-
+	
+	char str[] = "HalLL9O";
+	
+	printf("Lowercasing str: %s ",str);
+	lower_ascii(str);
+	printf("%s",str);
 }
